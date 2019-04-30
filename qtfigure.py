ad2016f49eab1160d706aaa10e0558ef703d3a06
mac_ilong <wenlong.li@vhall.com>
2019-04-30 10:19:46 +0800

    add x time

--- qtfigure.py
+++ qtfigure.py
@@ -4,6 +4,7 @@ import matplotlib
 matplotlib.use("Qt5Agg")  # 声明使用QT5
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
+import time
 
 class QtFigure(FigureCanvas):
     def __init__(self, width=5, height=4, dpi=100):
@@ -14,18 +15,24 @@ class QtFigure(FigureCanvas):
         # 第三步：创建一个子图，用于绘制图形用，111表示子图编号，如matlab的subplot(1,1,1)
         self.axes = self.fig.add_subplot(111)
         self.timer = QtCore.QTimer(self)
-        self.timer.timeout.connect(self.update_figure)
+        self.timer.timeout.connect(self.updateFigure)
         self.timer.start(1000)
 
     def plotSin(self):
         self.axes.grid(True, linestyle='--')
-        self.h, = self.axes.plot([0, 1, 2, 3], [1, 2, 0, 4], color='r', linewidth=0.5)
+        self.dateList = [0,0,0,0]
+        self.h, = self.axes.plot(self.dateList, [1, 2, 0, 4], color='r', linewidth=0.5)
 
-    def update_figure(self):
+    def updateFigure(self):
         # 构建4个随机整数，位于闭区间[0, 10]
         self.axes.cla()
         l = [np.random.randint(0, 10) for i in range(4)]
+        self.dateList.pop(0)
+        self.dateList.append(self.dateFormat())
         self.axes.grid(True, linestyle='--')
-        self.axes.plot([0, 1, 2, 3], l, color='r', linewidth=0.5)
+        self.axes.plot(self.dateList, l, color='r', linewidth=0.5)
         # self.h.set_ydata(l)
         self.draw()
+
+    def dateFormat(self):
+        return time.strftime("%H:%M:%S", time.localtime())
